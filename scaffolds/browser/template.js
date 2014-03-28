2bc34e15f35d741a50d5cc96b055e102d7be5f65
Greg Allen <me@jga.me>
2014-03-28 11:46:32 -0700

    auto prefix existing data when updating lib

--- scaffolds/browser/template.js
+++ scaffolds/browser/template.js
@@ -5,17 +5,37 @@ exports.warnOn = '*';
 
 exports.template = function(grunt, init, done) {
 
+  var defaults = {
+    name: '',
+    description: '',
+    version: '0.0.1',
+    homepage: '',
+    author_name: 'First+Third',
+    licenses: 'MIT'
+  }
+  if (grunt.ftInit.update) {
+    var path = require('path');
+    var bowerJson = require(path.join(process.cwd(), 'bower.json'));
+    defaults.name = bowerJson.name;
+    defaults.description = bowerJson.description;
+    defaults.version = bowerJson.version;
+    defaults.homepage = bowerJson.homepage;
+    defaults.author_name = bowerJson.copyright;
+    defaults.licenses = bowerJson.license;
+  }
+
+  console.log(defaults);
+
   init.process({ type: 'browser' }, [
-    init.prompt('name'),
-    init.prompt('description', ''),
-    init.prompt('version', '0.0.1'),
-    init.prompt('homepage'),
-    init.prompt('bugs'),
-    init.prompt('author_name', 'First + Third'),
-    init.prompt('licenses', 'MIT')
+    init.prompt('name', defaults.name),
+    init.prompt('description', defaults.description),
+    init.prompt('version', defaults.version),
+    init.prompt('homepage', defaults.homepage),
+    init.prompt('author_name', defaults.author_name),
+    init.prompt('licenses', defaults.licenses)
   ], function(err, props) {
 
-    props.ftInitVersion = grunt.ftInitVersion;
+    props.ftInit = grunt.ftInit;
     var files = init.filesToCopy(props);
     init.copyAndProcess(files, props);
     done();
